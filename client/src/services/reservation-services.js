6feded2d16574100dadd1cf14f3e28b1889b69a5
Jack <114423104+dig1234567@users.noreply.github.com>
2026-04-16 22:18:07 +0800

    Update reservation-services.js

--- client/src/services/reservation-services.js
+++ client/src/services/reservation-services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${process.env.REACT_APP_API_URL}/reservation`;
+const API_URL = `${process.env.REACT_APP_API_URL}/reservations`;
 
 // 訂位邏輯處理
 class Reservation {
