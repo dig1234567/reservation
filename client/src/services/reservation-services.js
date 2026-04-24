f9b183ac149de2044120a0e9b93c9d5d4affe790
Jack <114423104+dig1234567@users.noreply.github.com>
2026-04-24 19:48:46 +0800

    Update reservation-services.js

--- client/src/services/reservation-services.js
+++ client/src/services/reservation-services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${process.env.REACT_APP_API_URL}/reservations`;
+const API_URL = "https://reservation-1-tnsv.onrender.com/api/reservations";
 
 // 訂位邏輯處理
 class Reservation {
